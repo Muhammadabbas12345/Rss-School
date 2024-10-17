57df8284fbfe49c51e564ffbb90e447c31f43ee6
Muhammad Abbas <99185810+Muhammadabbas12345@users.noreply.github.com>
2024-10-18 03:15:09 +0500

    Revert "dashboard"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Dashboard from "@/components/Dashboard/Dashboard";
+import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <DefaultLayout>
-        <Dashboard />
+        <ECommerce />
       </DefaultLayout>
     </>
   );
