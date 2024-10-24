cbd205c6914df67481d8d717a9686b71337477f9
Muhammadabbas12345 <abbasarbab007@gmail.com>
2024-10-24 23:32:35 +0500

    ok

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,19 @@
-
-
 import React from 'react';
+import Image from 'next/image';
+
 
 function Homes() {
   return (
     <div className="min-h-screen bg-gray-100">
       {/* Header */}
-      <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">RSS School</h1>
+      <header className="bg-boxdark text-white p-2 flex justify-between items-center">
+        <Image
+          src= "/images/logo/logo.png"
+          alt="Right Step Montessori & High School Logo"
+          width={80}  
+          height={80}  
+          priority={true} 
+        />
         <div className="space-x-4">
           <a className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200"
           href='/dashboard'>
